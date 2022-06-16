7fa9cc90cc78c3338f0bb735b6c7c534b0fdbd4a
abajaj9 <50530932+abajaj9@users.noreply.github.com>
2022-06-16 10:25:31 -0400

    Initial commit

---
+++ src/main/java/com/doordash/doordash/DoordashApplication.java
@@ -0,0 +1,13 @@
+package com.doordash.doordash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoordashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoordashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doordash/doordash/DoordashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doordash.doordash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoordashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
